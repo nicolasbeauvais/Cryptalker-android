d4afe2e30c3d7fe7e2d3bed01ef45ece8e5c6aba
Nicolas Beauvais <nicolas@bvs.email>
2014-11-04 10:22:26 +0100

    Update project configuration files with PullToRefresh external lib

--- src/tk/cryptalker/HomeActivity.java
+++ src/tk/cryptalker/HomeActivity.java
@@ -7,6 +7,7 @@ public class HomeActivity extends AbstractActivity
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
 
         context = getApplicationContext();
